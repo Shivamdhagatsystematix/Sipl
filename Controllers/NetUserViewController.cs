c63e3fc4b594ab45149ef6ec61c18a938d707f8e
Shivamdhagatsystematix <shivam.dhagat@systematixindia.com>
2018-09-27 18:50:27 +0530

    TWO model
    TWO model

---
+++ Controllers/NetUserViewController.cs
@@ -0,0 +1,167 @@
+﻿using Sipl.DataBase;
+using Sipl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Sipl.Controllers
+{
+    public class NetUserViewController : Controller
+
+    {
+        SipDatabaseEntities objEntities = new SipDatabaseEntities();
+
+        // GET: NetUserView
+        public ActionResult Index()
+        {
+            SipDatabaseEntities objEntities = new SipDatabaseEntities();
+            List<NetUserViewModel> objNetUserViewModel = new List<NetUserViewModel>();
+            var data = (from p in objEntities.NetUsers select p).ToList();
+            foreach (var item in data)
+            {
+                NetUserViewModel netUser = new NetUserViewModel
+                {
+                    UserId = item.UserId,
+                    FirstName = item.FirstName,
+                    LastName = item.LastName,
+                    Gender = item.Gender,
+                    Email = item.Email,
+                    Password = item.Password,
+                    ConfirmPassword = item.ConfirmPassword,
+                    DOB = item.DOB,
+                    IsActive = item.IsActive,
+                    DateCreated = item.DateCreated,
+                    DateModified = item.DateModified
+                };
+
+                objNetUserViewModel.Add(netUser);
+
+            };
+            return View(objNetUserViewModel);
+        }
+
+        // GET: NetUserView/Details/5 
+        public ActionResult  Details (int ?id)
+        {
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                NetUsers netUsers = objEntities.NetUsers.Find(id);
+                var data = from d in objEntities.NetUsers
+                                   where d.UserId == id
+                                   select d;
+                var TEMPlIST = objEntities.NetUsers.ToList();
+
+                NetUserViewModel netUser = new NetUserViewModel
+                {
+                    UserId = netUsers.UserId,
+                    FirstName = netUsers.FirstName,
+                    LastName = netUsers.LastName,
+                    Gender = netUsers.Gender,
+                    Email = netUsers.Email,
+                    Password = netUsers.Password,
+                    ConfirmPassword = netUsers.ConfirmPassword,
+                    DOB = netUsers.DOB,
+                    IsActive = netUsers.IsActive,
+                    DateCreated = netUsers.DateCreated,
+                    DateModified = netUsers.DateModified
+                };
+
+               
+
+            if (netUsers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(netUser);
+        }
+    }
+
+        // GET: NetUserView/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NetUserView/Create
+       
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "UserId,FirstName,LastName," +
+            "Role,Gender,Email,Password,ConfirmPassword,DOB" +
+            ",IsActive,DateCreated,DateModified")] NetUsers netUsers)
+        {
+            try
+            {
+                NetUserViewModel netUser = new NetUserViewModel
+                    
+
+                {
+                    if (ModelState.IsValid)
+                    {
+                        db.NetUsers.Add(netUsers);
+                       db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+
+                 
+
+
+                    return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return View(netUsers);
+            }
+        }
+
+        // GET: NetUserView/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NetUserView/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NetUserView/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NetUserView/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/NetUserViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sipl.Models
+{
+    
+    public class NetUserViewModel
+    {
+        //public Entities db = new Entities();
+
+        public byte UserId { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+
+        public string LastName { get; set; }
+        [Required]
+
+        public string Role { get; set; }
+
+
+        public string Gender { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "DOB")]
+        public System.DateTime DOB { get; set; }
+        public bool IsActive { get; set; }
+        public System.DateTime DateCreated { get; set; }
+        public System.DateTime DateModified { get; set; }
+
+       
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<Address> Addresses { get; set; }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<TeacherInSubject> TeacherInSubjects { get; set; }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<UserRole> UserRoles { get; set; }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<UserRole> UserRoles1 { get; set; }
+    }
+
+    
+}

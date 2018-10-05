295d8cdb1d0aa551f719c8fab5c5bb7d933ece37
Shivamdhagatsystematix <shivam.dhagat@systematixindia.com>
2018-10-05 17:09:35 +0530

    login page and loggin page

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using Sipl.DataBase;
+using Sipl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sipl.Controllers
+{
+    public class LogInController : Controller
+    {
+    
+
+        // GET: Login/Create
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        // POST: Login/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(NetUsers objNetUsers)
+        {
+            try
+            {
+                if (objNetUsers.Email != null && objNetUsers.Password != null)
+                {
+                    using (SiplDatabaseEntities objSiplDatabaseEntities = new SiplDatabaseEntities())
+                    {
+                        var obj = objSiplDatabaseEntities.NetUsers.Where(u => u.Email.Equals
+                        (objNetUsers.Email) && u.Password.Equals(objNetUsers.Password)).FirstOrDefault();
+                        if (obj != null)
+                        {
+                            Session["Email"] = obj.Email.ToString();
+                            Session["Password"] = obj.Password.ToString();
+                            return RedirectToAction("LoggedIn");
+                        }
+                    }
+                }
+
+                        return View(objNetUsers);
+
+
+        }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult LoggedIn()
+        {
+            if (Session["Email"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+
+
+
+
+    }
+}
--- DataBase/SipDatabase.Context.cs
+++ DataBase/SipDatabase.Context.cs
@@ -39,7 +39,8 @@ namespace Sipl.DataBase
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<TeacherInSubject> TeacherInSubject { get; set; }
         public virtual DbSet<UserRole> UserRole { get; set; }
-    
+     
+
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
             var diagramnameParameter = diagramname != null ?

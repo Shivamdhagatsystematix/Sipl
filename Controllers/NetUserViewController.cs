11c7bcfed2cbacb7c40912f292aacb1d0e86c952
Shivamdhagatsystematix <shivam.dhagat@systematixindia.com>
2018-10-11 17:36:42 +0530

    Coding Syntax

--- Controllers/NetUserViewController.cs
+++ Controllers/NetUserViewController.cs
@@ -10,27 +10,26 @@ using System.Web.Mvc;
 
 namespace Sipl.Controllers
 {
-
     public class NetUserViewController : Controller
-
     {
         SiplDatabaseEntities objEntities = new SiplDatabaseEntities();
 
-
         /// <summary>
-        /// Index of all Registered User 
+        /// List of all Registered User 
         /// </summary>
         /// <returns></returns>
-        public ActionResult Index()
-
+        public ActionResult RegisteredUserList()
         {
+            // Get user roles from DB
             ViewBag.Role = new SelectList(objEntities.NetRoles.ToList(), "RoleId", "RoleName");
             List<NetUserViewModel> objNetUserViewModel = new List<NetUserViewModel>();
+
+            //Get List of users from DB
             var data = (from p in objEntities.NetUsers select p).ToList();
             foreach (var item in data)
             {
+                //To get Address From DB
                 var userAddressInfo = (from p in objEntities.Address where p.UserId == item.UserId select p).FirstOrDefault();
-
                 if (userAddressInfo != null)
                 {
                     NetUserViewModel netUser = new NetUserViewModel
@@ -49,34 +48,31 @@ namespace Sipl.Controllers
                         States = userAddressInfo.States.StateName,
                         Cities = userAddressInfo.Cities.CityName,
                         DateCreated = item.DateCreated
-
                     };
                     objNetUserViewModel.Add(netUser);
                 }
-
             };
             return View(objNetUserViewModel);
         }
 
-
-       /// <summary>
-       /// Details of User
-       /// </summary>
-       /// <param name="id"></param>
-       /// <returns></returns>
-        public ActionResult Details(int? id)
+        /// <summary>
+        /// Details of Registered Users
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult RegisteredUserDetails(int? id)
         {
             {
                 if (id == null)
                 {
                     return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
                 }
+                //To Find Details of Particular Registered User
                 NetUsers netUsers = objEntities.NetUsers.Find(id);
                 var data = from d in objEntities.NetUsers
                            where d.UserId == id
                            select d;
-                var TEMPlIST = objEntities.NetUsers.ToList();
-
+                //var TEMPlIST = objEntities.NetUsers.ToList();
                 NetUserViewModel netUser = new NetUserViewModel
                 {
                     UserId = netUsers.UserId,
@@ -92,17 +88,15 @@ namespace Sipl.Controllers
                 };
                 return View(netUser);
             }
-
         }
 
         /// <summary>
-        ///Register Method For Users
+        ///GET :Registration Method For Users
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         public ActionResult RegisterUser()
         {
-
             //TO GET ROLES FROM DATABASE
             var Roles = (from b in objEntities.NetRoles select b).ToList();
             var model = new NetUserViewModel
@@ -113,7 +107,6 @@ namespace Sipl.Controllers
                     Text = x.RoleName
                 })
             };
-
             //GET : COURSE FOR USERS
             var course = (from b in objEntities.Courses select b).ToList();
             model.Course = course.Select(x => new SelectListItem
@@ -121,32 +114,30 @@ namespace Sipl.Controllers
                 Value = x.CourseId.ToString(),
                 Text = x.CourseName
             });
-
             //TO GET COUNTRY ,STATES AND CITY
             {
                 var country = objEntities.Countries.ToList();
-                List<SelectListItem> liCountry = new List<SelectListItem>();
-                List<SelectListItem> liState = new List<SelectListItem>();
-                List<SelectListItem> liCity = new List<SelectListItem>();
+                List<SelectListItem> listCountry = new List<SelectListItem>();
+                List<SelectListItem> listState = new List<SelectListItem>();
+                List<SelectListItem> listCity = new List<SelectListItem>();
 
-                liCountry.Add(new SelectListItem { Text = "", Value = "0" });
-                liState.Add(new SelectListItem { Text = "", Value = "0" });
-                liCity.Add(new SelectListItem { Text = "", Value = "0" });
+                listCountry.Add(new SelectListItem { Text = "", Value = "0" });
+                listState.Add(new SelectListItem { Text = "", Value = "0" });
+                listCity.Add(new SelectListItem { Text = "", Value = "0" });
 
                 foreach (var m in country)
                 {
-                    liCountry.Add(new SelectListItem { Text = m.CountryName, Value = m.CountryId.ToString() });
+                    listCountry.Add(new SelectListItem { Text = m.CountryName, Value = m.CountryId.ToString() });
                 }
-                ViewBag.country = liCountry;
-                ViewBag.State = liState;
-                ViewBag.City = liCity;
-
+                ViewBag.country = listCountry;
+                ViewBag.State = listState;
+                ViewBag.City = listCity;
             }
             return View(model);
         }
 
         /// <summary>
-        /// Register Method For Users
+        /// Post: Register Method For Users
         /// </summary>
         /// <param name="objNetUserViewModel"></param>
         /// <returns></returns>
@@ -163,6 +154,7 @@ namespace Sipl.Controllers
                     //Encrytion For Password
                     var keyNew = "Test";
                     var password = Helper.EncodePassword(objNetUserViewModel.Password, keyNew);
+
                     NetUsers objNetUsers = new NetUsers
                     {
                         FirstName = objNetUserViewModel.FirstName,
@@ -191,7 +183,6 @@ namespace Sipl.Controllers
                     objEntities.UserRole.Add(objUserRole);
                     objEntities.SaveChanges();
 
-
                     //to add data in Address Table
                     Address objAddress = new Address
                     {
@@ -201,7 +192,6 @@ namespace Sipl.Controllers
                         CurrentAddress = objNetUserViewModel.CurrentAddress,
                         PermanantAddress = objNetUserViewModel.PermanantAddress,
                         UserId = userId
-
                     };
                     objEntities.Address.Add(objAddress);
                     objEntities.SaveChanges();
@@ -221,7 +211,7 @@ namespace Sipl.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public ActionResult Edit(int id)
+        public ActionResult UpdateRegisteredUsers(int id)
         {
             ViewBag.Role = new SelectList(objEntities.NetRoles.ToList(), "RoleId", "RoleName");
             {
@@ -234,7 +224,6 @@ namespace Sipl.Controllers
                            where d.UserId == id
                            select d;
                 var TEMPlIST = objEntities.NetUsers.ToList();
-
                 NetUserViewModel netUser = new NetUserViewModel
                 {
                     FirstName = netUsers.FirstName,
@@ -262,7 +251,7 @@ namespace Sipl.Controllers
         /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(NetUserViewModel objNetUserViewModel)
+        public ActionResult UpdateRegisteredUsers(NetUserViewModel objNetUserViewModel)
         {
             ViewBag.Role = new SelectList(objEntities.NetRoles.ToList(), "RoleId", "RoleName");
             try
@@ -271,7 +260,6 @@ namespace Sipl.Controllers
                 {
                     NetUsers objNetUsers = new NetUsers
                     {
-
                         FirstName = objNetUserViewModel.FirstName,
                         LastName = objNetUserViewModel.LastName,
                         Gender = objNetUserViewModel.Gender,
@@ -282,11 +270,9 @@ namespace Sipl.Controllers
                         DateCreated = DateTime.Now,
                         DateModified = DateTime.Now
                     };
-
                     objEntities.NetUsers.Add(objNetUsers);
                     objEntities.SaveChanges();
                     return RedirectToAction("Index");
-
                 }
                 return View(objNetUserViewModel);
             }
@@ -301,7 +287,7 @@ namespace Sipl.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public ActionResult Delete(int id)
+        public ActionResult DeleteRegisteredUsers(int id)
         {
             {
                 if (id == null)
@@ -313,10 +299,8 @@ namespace Sipl.Controllers
                            where d.UserId == id
                            select d;
                 var TEMPlIST = objEntities.NetUsers.ToList();
-
                 NetUserViewModel netUser = new NetUserViewModel
                 {
-
                     FirstName = netUsers.FirstName,
                     LastName = netUsers.LastName,
                     Gender = netUsers.Gender,
@@ -327,7 +311,6 @@ namespace Sipl.Controllers
                     DateCreated = netUsers.DateCreated,
                     DateModified = netUsers.DateModified
                 };
-
                 if (netUsers == null)
                 {
                     return HttpNotFound();
@@ -343,7 +326,7 @@ namespace Sipl.Controllers
         /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(byte id)
+        public ActionResult DeleteRegisteredUsers(byte id)
         {
             try
             {
@@ -366,12 +349,11 @@ namespace Sipl.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public JsonResult getstate(int id)
+        public JsonResult Getstate(int id)
         {
             var states = objEntities.States.Where(x => x.CountryId == id).ToList();
             List<SelectListItem> listates = new List<SelectListItem>();
             listates.Add(new SelectListItem { Text = "", Value = "0" });
-
             if (states != null)
             {
                 foreach (var x in states)
@@ -387,11 +369,10 @@ namespace Sipl.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public JsonResult getCity(int id)
+        public JsonResult GetCity(int id)
         {
             var city = objEntities.Cities.Where(x => x.StateId == id).ToList();
             List<SelectListItem> licity = new List<SelectListItem>();
-
             licity.Add(new SelectListItem { Text = "", Value = "0" });
             if (city != null)
             {
@@ -400,7 +381,6 @@ namespace Sipl.Controllers
                     licity.Add(new SelectListItem { Text = l.CityName, Value = l.CityId.ToString() });
                 }
             }
-
             return Json(new SelectList(licity, "Value", "Text", JsonRequestBehavior.AllowGet));
         }
     }
